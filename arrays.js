bf9840fbaa18997166a11409fb28fe1a6a2d79ab
Lassana Magassa <mystuff@lassanamagassa.com>
2018-06-03 01:07:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(theArray, elementToAdd) {
+  //theArray.unshift(elementToAdd)
+  var spreadArray = [elementToAdd, ...theArray]
+  return spreadArray
+  
+}
+
+
